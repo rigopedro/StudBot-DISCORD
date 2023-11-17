93518215e578c2ca51053859a760a76e91680f2c
rigopedro <pedrorigooliveira@hotmail.com>
2023-11-17 18:33:18 -0300

    Estilização das respostas

--- commands/git.js
+++ commands/git.js
@@ -7,28 +7,32 @@ module.exports = {
 
   async execute(interaction) {
     await interaction.reply({
-      content: 'Comandos do Git 🚀',
       embeds: [
         {
-          color: 0x5865F2, 
-          title: 'Comandos do Git 🚀',
+          color: 0x5865F2, // Cor BLURPLE
           description: 'Relembrar comandos do Git e suas funcionalidades.',
           fields: [
             {
               name: 'Comandos Básicos',
-              value: '`git init [nome-do-projeto]` - Cria um novo repositório local com um nome especificado.',
+              value: '🚀 `git init [nome-do-projeto]` - Cria um novo repositório local com um nome especificado.',
             },
             {
               name: 'Comandos de Status e Commit',
-              value: '`git status` - Revise edições e crie uma transação de commit.',
+              value: 'ℹ️ `git status` - Revise edições e crie uma transação de commit.\n' +
+                     '➕ `git add [arquivo]` - Faz o snapshot de um arquivo na preparação para versionamento.\n' +
+                     '✅ `git commit -m "[mensagem]"` - Grava o snapshot permanentemente do arquivo no histórico de versão.',
             },
             {
               name: 'Comandos de Branch',
-              value: '`git branch` - Lista todos os branches locais no repositório atual.',
+              value: '🌿 `git branch` - Lista todos os branches locais no repositório atual.\n' +
+                     '➕ `git branch [nome-branch]` - Cria uma nova branch.\n' +
+                     '🔄 `git switch -c [nome-branch]` - Muda para a branch especificada e atualiza o diretório de trabalho.',
             },
             {
               name: 'Comandos de Merge e Push',
-              value: '`git merge [nome-branch]` - Combina o histórico da branch especificada à branch atual.',
+              value: '🔀 `git merge [nome-branch]` - Combina o histórico da branch especificada à branch atual.\n' +
+                     '📤 `git push [alias] [branch]` - Envia todos os commits do branch local para o GitHub.\n' +
+                     '📥 `git pull` - Baixa o histórico e incorpora as mudanças.',
             },
           ],
         },
