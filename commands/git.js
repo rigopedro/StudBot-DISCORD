c568e278484cae6166114cc4c6cb9b75d3ad6b3d
rigopedro <pedrorigooliveira@hotmail.com>
2023-11-17 18:18:17 -0300

    Estilização da resposta

--- commands/git.js
+++ commands/git.js
@@ -1,32 +1,38 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
-const exampleEmbed = new EmbedBuilder()
-	.setColor(0x0099FF)
-	.setTitle('Some title')
-	.addFields(
-        { name: '\u200B', value: '\u200B' },
-		{ name: '$ git init [nome-do-projeto]', value: 'Cria um novo repositório local com um nome especificado', inline: true },
-		{ name: '$ git clone [url]', value: 'Baixa um projeto e seu histórico de versão inteiro', inline: true },
-		{ name: '$ git stash', value: 'Armazena temporariamente todos os arquivos monitorados modificados', inline: true },
-		{ name: '\u200B', value: '\u200B' },
-		{ name: '$ git status', value: 'Revise edições e crie uma transação de commit', inline: true },
-		{ name: '$ git add [arquivo]', value: 'Faz o snapshot de um arquivo na preparação para versionamento', inline: true },
-		{ name: '$ git commit -m "[mensagem]"', value: 'Grava o snapshot permanentemente do arquivo no histórico de versão', inline: true },
-		{ name: '\u200B', value: '\u200B' },
-		{ name: '$ git branch', value: 'Lista todos os branches locais no repositório atual', inline: true },
-		{ name: '$ git branch [nome-branch]', value: 'Cria uma nova branch', inline: true },
-		{ name: '$ git switch -c [nome-branch]', value: 'Muda para a branch especificada e atualiza o diretório de trabalho', inline: true },
-		{ name: '\u200B', value: '\u200B' },
-		{ name: '$ git merge [nome-branch]', value: 'Combina o histórico da branch especificada a branch atual', inline: true },
-		{ name: '$ git push [alias] [branch]', value: 'Envia todos os commits do branch local para o GitHub', inline: true },
-		{ name: '$ git pull', value: 'Baixa o histórico e incorpora as mudanças', inline: true },
-	)
+
+const gitCommandsEmbed = new EmbedBuilder()
+  .setColor(0x0099FF)
+  .setTitle('Comandos do Git 🚀')
+  .setDescription('Relembrar comandos do Git e suas funcionalidades.');
+
+gitCommandsEmbed
+  .addField('Comandos Básicos', [
+    { name: '$ git init [nome-do-projeto]', value: 'Cria um novo repositório local com um nome especificado.' },
+    { name: '$ git clone [url]', value: 'Baixa um projeto e seu histórico de versão inteiro.' },
+    { name: '$ git stash', value: 'Armazena temporariamente todos os arquivos monitorados modificados.' },
+  ])
+  .addField('Comandos de Status e Commit', [
+    { name: '$ git status', value: 'Revise edições e crie uma transação de commit.' },
+    { name: '$ git add [arquivo]', value: 'Faz o snapshot de um arquivo na preparação para versionamento.' },
+    { name: '$ git commit -m "[mensagem]"', value: 'Grava o snapshot permanentemente do arquivo no histórico de versão.' },
+  ])
+  .addField('Comandos de Branch', [
+    { name: '$ git branch', value: 'Lista todos os branches locais no repositório atual.' },
+    { name: '$ git branch [nome-branch]', value: 'Cria uma nova branch.' },
+    { name: '$ git switch -c [nome-branch]', value: 'Muda para a branch especificada e atualiza o diretório de trabalho.' },
+  ])
+  .addField('Comandos de Merge e Push', [
+    { name: '$ git merge [nome-branch]', value: 'Combina o histórico da branch especificada à branch atual.' },
+    { name: '$ git push [alias] [branch]', value: 'Envia todos os commits do branch local para o GitHub.' },
+    { name: '$ git pull', value: 'Baixa o histórico e incorpora as mudanças.' },
+  ]);
 
 module.exports = {
-    data: new SlashCommandBuilder()
-         .setName("git")
-        .setDescription("Relembrar comandos do Git"),
+  data: new SlashCommandBuilder()
+    .setName('git')
+    .setDescription('Relembrar comandos do Git'),
 
-    async execute(interaction) {
-      await interaction.reply({ embeds: [exampleEmbed] })
-    }
-}
+  async execute(interaction) {
+    await interaction.reply({ embeds: [gitCommandsEmbed] });
+  },
+};
