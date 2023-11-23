21b616a2a7e9ef09e56903ec444fe6cdfc740f2e
rigopedro <pedrorigooliveira@hotmail.com>
2023-11-22 22:33:51 -0300

    Atualização nas informações do bot

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -1,10 +1,9 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder } = require('discord.js');
 
 module.exports = {
-    data: {
-        name: 'botinfo',
-        description: 'Informações sobre o bot e como ele pode ajudar no estudo da programação.',
-    },
+    data: new SlashCommandBuilder()
+        .setName('botinfo')
+        .setDescription('Informações sobre o bot e como ele pode ajudar no estudo da programação.'),
     async execute(interaction) {
         await interaction.reply('O bot é uma ferramenta interativa para auxiliar no estudo de programação. Ele fornece informações sobre diversas linguagens e comandos úteis. Se tiver dúvidas ou quiser ver todos os comandos, use /cmd.');
     },
